420195a9c3d59c0c4a602a395688cd12d9774961
alif <hexc@protonmail.com>
2023-12-10 21:24:58 +0200

    gstreamed_ort: non-working test version on sample image

--- gstreamed_ort/src/main.rs
+++ gstreamed_ort/src/main.rs
@@ -1,3 +1,93 @@
-fn main() {
-    println!("Hello, world!");
+mod yolo_parser;
+
+use crate::yolo_parser::report_detect;
+use image::imageops::{resize, FilterType};
+use image::{DynamicImage, GenericImageView, RgbImage};
+use ndarray::{Array, Array4, CowArray, Dimension};
+use ort::tensor::OrtOwnedTensor;
+use ort::{ExecutionProvider, GraphOptimizationLevel, OrtResult, SessionBuilder, Value};
+
+fn image_from_ndarray<D: Dimension>(
+    array: Array<f32, D>,
+    width: u32,
+    height: u32,
+) -> Option<RgbImage> {
+    RgbImage::from_vec(
+        width,
+        height,
+        array.into_raw_vec().into_iter().map(|v| v as u8).collect(),
+    )
+}
+
+fn main() -> anyhow::Result<()> {
+    let ort_env = ort::Environment::builder()
+        .with_name("yolov8")
+        .with_execution_providers([ExecutionProvider::CPU(Default::default())])
+        .build()?
+        .into_arc();
+
+    let session = SessionBuilder::new(&ort_env)?
+        .with_optimization_level(GraphOptimizationLevel::Level1)?
+        .with_intra_threads(1)?
+        .with_model_from_file("_models/yolov8s.640x360.cpu.onnx")?;
+    println!("session: {session:?}");
+
+    // read image
+    let image = image::open("sample.jpg").unwrap();
+
+    println!("image.dimensions: {:?}", image.dimensions());
+    println!("image.color: {:?}", image.color());
+    let image = image.into_rgb8();
+
+    // resize image to fit what we got
+    // our model is 640 x 384
+    let scaled_image = resize(&image, 640, 384, FilterType::CatmullRom);
+
+    // copy image into ndarray
+    // TODO do we need to normalize to 0..1, when converting u8 to f32?
+    // and permute axes to match inputs of our model
+    let image_array = Array4::from_shape_vec(
+        (1, 640, 384, 3),
+        scaled_image
+            .into_raw()
+            .into_iter()
+            // cast from u8 to f32 and convert color from [0; 255] to [0.0; 1.0]
+            .map(|v| (v as f32) * (1.0 / 255.0))
+            .collect(),
+    )
+    .unwrap()
+    .permuted_axes([0, 3, 2, 1]);
+
+    // check that image still makes sense
+    let test_image = image_from_ndarray(image_array.clone(), 640, 384).unwrap();
+    test_image.save("test.jpg").unwrap();
+
+    let image_array = CowArray::from(image_array).into_dyn();
+    println!("image_array.shape: {:?}", image_array.shape());
+    // read into ndarray
+    // let input: &CowArray = wrapped.into();
+    let input = vec![Value::from_array(session.allocator(), &image_array)?];
+
+    // and run
+    let outputs: Vec<Value> = session.run(input)?;
+    let outputs: OrtOwnedTensor<f32, _> = outputs[0].try_extract()?;
+    let output_view = outputs.view();
+    // output shape is 1 x 84 x 5040
+    println!("got outputs: {outputs:?}");
+    // parse and annotate outputs
+    let img = report_detect(
+        output_view.view(),
+        DynamicImage::ImageRgb8(image),
+        640,
+        384,
+        0.25,
+        0.45,
+        14,
+    )
+    .unwrap();
+    img.save("output.jpg")?;
+
+    // TODO warmup with synthetic image of the same dims
+
+    Ok(())
 }
---
+++ gstreamed_ort/src/yolo_parser.rs
@@ -0,0 +1,76 @@
+use candle_transformers::object_detection::{non_maximum_suppression, Bbox, KeyPoint};
+use gstreamed_common::annotate::annotate_image_with_bboxes;
+use image::{DynamicImage, RgbImage};
+use ndarray::{s, ArrayView, IxDyn};
+
+fn parse_predictions(
+    preds: ArrayView<f32, IxDyn>,
+    conf_threshold: f32,
+    nms_threshold: f32,
+) -> Vec<Vec<Bbox<Vec<KeyPoint>>>> {
+    // in candle case we have:
+    // initial pred.shape: [84, 4620]
+    // pred.shape: [84]
+    // pred.len(): 84
+    let shape = preds.shape();
+    println!("preds.shape: {shape:?}");
+    // TODO study wtf they doing in ultralytics yolo rust example
+    let (pred_size, npreds) = (shape[1], shape[2]);
+    let nclasses = pred_size - 4;
+    // The bounding boxes grouped by (maximum) class index.
+    let mut bboxes: Vec<Vec<Bbox<Vec<KeyPoint>>>> = (0..nclasses).map(|_| vec![]).collect();
+    // Extract the bounding boxes for which confidence is above the threshold.
+    for index in 0..npreds {
+        let pred_view = preds.slice(s![.., .., index]);
+        println!("pred_view.shape: {:?}", pred_view.shape());
+        let pred: Vec<f32> = pred_view.iter().copied().collect();
+        // println!("pred: {pred:?}");
+        // std::process::exit(0);
+        let confidence = *pred[4..].iter().max_by(|x, y| x.total_cmp(y)).unwrap();
+        if confidence > conf_threshold {
+            let mut class_index = 0;
+            for i in 0..nclasses {
+                if pred[4 + i] > pred[4 + class_index] {
+                    class_index = i
+                }
+            }
+            if pred[class_index + 4] > 0. {
+                let bbox = Bbox {
+                    xmin: pred[0] - pred[2] / 2.,
+                    ymin: pred[1] - pred[3] / 2.,
+                    xmax: pred[0] + pred[2] / 2.,
+                    ymax: pred[1] + pred[3] / 2.,
+                    confidence,
+                    data: vec![],
+                };
+                if bbox.xmin < 0.0 || bbox.ymin < 0.0 || bbox.xmax < 0.0 || bbox.ymax < 0.0 {
+                    eprintln!("bbox with negative coords: {bbox:?}");
+                    eprintln!("from preds: {pred:?}");
+                } else {
+                    bboxes[class_index].push(bbox)
+                }
+            }
+        }
+    }
+    // std::process::exit(0);
+    // nms
+    non_maximum_suppression(&mut bboxes, nms_threshold);
+    bboxes
+}
+
+/// Equivalent of report_detect for candle, but using ndarray ArrayView as input
+/// so this can be used for ort predictions.
+pub fn report_detect(
+    pred_view: ArrayView<f32, IxDyn>,
+    img: DynamicImage,
+    w: usize,
+    h: usize,
+    confidence_threshold: f32,
+    nms_threshold: f32,
+    legend_size: u32,
+) -> anyhow::Result<DynamicImage> {
+    let bboxes = parse_predictions(pred_view, confidence_threshold, nms_threshold);
+
+    // Annotate the original image and print boxes information.
+    Ok(annotate_image_with_bboxes(img, w, h, legend_size, &bboxes))
+}
